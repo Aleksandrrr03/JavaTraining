102f7628c897f803bad5829a4d77f5feb44b764e
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-05-26 20:43:45 +0300

    Add files via upload

---
+++ lesson3/task2/starter/Starter.java
@@ -0,0 +1,9 @@
+package task2.starter;
+
+import task2.valueExchangeTester.*;
+
+public class Starter {
+    public static void main(String[] args) {
+        valueExchangeTesting.valueExchangeTester();
+    }
+}
