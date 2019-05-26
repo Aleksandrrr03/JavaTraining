cd5312ed941062357bdd069ead1b7221a75256fb
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-05-26 20:44:48 +0300

    class Starter

--- lesson3/task2/starter/Starter.java
+++ lesson3/task2/starter/Starter.java
@@ -3,6 +3,7 @@ package task2.starter;
 import task2.valueExchangeTester.*;
 
 public class Starter {
+    
     public static void main(String[] args) {
         valueExchangeTesting.valueExchangeTester();
     }
