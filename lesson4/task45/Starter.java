d8bcd1c704719c5a098be656f17c2f75ffa4f182
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-05-27 01:39:13 +0300

    Add files via upload

---
+++ lesson4/task45/Starter.java
@@ -0,0 +1,9 @@
+package task45;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        System.out.printf("Number = %d" + "\n", Logic.NUMBER);
+        System.out.printf("Product of numbers  = %d" + "\n", Logic.findProduct());
+    }
+}
