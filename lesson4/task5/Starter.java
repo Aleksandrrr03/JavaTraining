933869d794e69ce160ec06de41f015820d7d12ea
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-05-27 01:44:08 +0300

    Delete Starter.java

--- lesson4/task5/Starter.java
+++
@@ -1,9 +0,0 @@
-package task5;
-
-public class Starter {
-    public static void main(String[] args) {
-
-        System.out.printf("Number = %d" + " \n",Logic.NUMBER);
-        System.out.printf("FlipNumber = %d"+ "\n", Logic.flipTheNumber());
-    }
-}
