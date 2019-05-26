8e6abbe69a6c9e33607705f2deeb6c91d6f0a51f
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-05-26 20:46:55 +0300

    Add files via upload

---
+++ lesson3/task2/valueExchangeTesting/valueExchangeTesting.java
@@ -0,0 +1,22 @@
+package task2.valueExchangeTester;
+
+import task2.valueExchange.valueExchange;
+
+public class valueExchangeTesting {
+
+    public static void valueExchangeTester() {
+
+        int[] array = {2, 5};
+
+        System.out.println("\n****The first way to exchange values****");
+        System.out.printf("Before the exchange of values: a=%d and b=%d " + "\n", array[0], array[1]);
+        array = valueExchange.exchangeValuesFirstWay(array);
+        System.out.printf("After the exchange of values: a=%d and b=%d" + "\n", array[0], array[1]);
+
+        System.out.println("\n****The second way to exchange values****");
+        System.out.printf("Before the exchange of values: a=%d and b=%d " + "\n", array[0], array[1]);
+        array = valueExchange.exchangeValuesSecondWay(array);
+        System.out.printf("After the exchange of values: a=%d and b=%d" + "\n", array[0], array[1]);
+
+    }
+}
