130388d4231e91e4b8a08c8a78c2c23e6f9753fa
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-05-27 01:38:11 +0300

    Add files via upload

---
+++ lesson4/task5/Starter.java
@@ -0,0 +1,9 @@
+package task5;
+
+public class Starter {
+    public static void main(String[] args) {
+
+        System.out.printf("Number = %d" + " \n",Logic.NUMBER);
+        System.out.printf("FlipNumber = %d"+ "\n", Logic.flipTheNumber());
+    }
+}
