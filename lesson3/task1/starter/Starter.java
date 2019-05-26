580701d90317b61ae6db88d929632840966b7592
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-05-26 19:34:07 +0300

    Add files via upload

---
+++ lesson3/task1/starter/Starter.java
@@ -0,0 +1,9 @@
+package starter;
+import conversionTesting.СonversionTesting;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        СonversionTesting.testConversion();
+    }
+}
