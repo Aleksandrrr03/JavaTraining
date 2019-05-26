f6c4de5fc0a190138213233f0c92733269093e82
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-05-26 21:52:54 +0300

    Add files via upload

---
+++ lesson3/task3/starter/Starter.java
@@ -0,0 +1,11 @@
+package task3.starter;
+
+
+import task3.massTransferTester.MassTransferTester;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        MassTransferTester.massTransferTesting();
+    }
+}
