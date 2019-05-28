1520c4fe264fea554f58fcaf04ce44283c515eff
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-05-28 14:45:00 +0300

    Update NumberComparison.java

--- lesson4/task1/NumberComparison.java
+++ lesson4/task1/NumberComparison.java
@@ -3,12 +3,6 @@ package task1;
 public class NumberComparison {
 
     public static boolean compareNumbers(float a, float b, float c) {
-
-        if (a == b && b == c && a == c) {
-            return true;
-        } else {
-            return false;
-        }
-
+        return a == b && b == c && a == c;
     }
 }
