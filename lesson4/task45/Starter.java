f9922997c62966b980a5f8d71a483769eb317496
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-05-28 15:27:23 +0300

    Update Starter.java

--- lesson4/task45/Starter.java
+++ lesson4/task45/Starter.java
@@ -3,7 +3,9 @@ package task45;
 public class Starter {
 
     public static void main(String[] args) {
-        System.out.printf("Number = %d" + "\n", Logic.NUMBER);
-        System.out.printf("Product of numbers  = %d" + "\n", Logic.findProduct());
+        int number = 232_235;
+
+        System.out.printf("Number = %d" + "\n", number);
+        System.out.printf("Product of numbers  = %d" + "\n", Logic.findProduct(number));
     }
 }
