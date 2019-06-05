a9db96d3eab6112f5c95a2d299e0638c8ea293bb
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-06-05 14:05:03 +0300

    Update DragonLogic.java

--- lesson5/src/by/epam/javatraining/kotkovets/lesson5/task01/model/logic/DragonLogic.java
+++ lesson5/src/by/epam/javatraining/kotkovets/lesson5/task01/model/logic/DragonLogic.java
@@ -15,15 +15,11 @@ public class DragonLogic {
 
     public static int countHeads(int age) {
 
-        int numberOfHeads;
+        int numberOfHeads = 0;
 
-        if (age < ALLOWABLE_AGE) {
-            numberOfHeads = 0;
-        } else {
+        if (age >= ALLOWABLE_AGE) {
             numberOfHeads = FIRST_NUMBER_OF_HEADS;
-
             if (age == ALLOWABLE_AGE) {
-
                 return numberOfHeads;
             } else if (age <= FIRST_AGE_LIMIT) {
                 numberOfHeads += age * FIRST_NUMBER_OF_HEADS;
