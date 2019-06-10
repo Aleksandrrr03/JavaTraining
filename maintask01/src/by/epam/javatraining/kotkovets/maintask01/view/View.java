14aa8e3125a3a1989f7b5c8d54fd70fb6b8a4db9
Aleksandrrr03 <47798887+Aleksandrrr03@users.noreply.github.com>
2019-06-10 12:52:27 +0300

    Update View.java

--- maintask01/src/by/epam/javatraining/kotkovets/maintask01/view/View.java
+++ maintask01/src/by/epam/javatraining/kotkovets/maintask01/view/View.java
@@ -3,11 +3,11 @@ package by.epam.javatraining.kotkovets.maintask01.view;
 import java.util.Arrays;
 
 public class View {
-    public static void PrintMSG(String msg){
+    public static void printMSG(String msg){
         System.out.print(msg);
     }
 
-    public static void PrintVector(double[] vector){
+    public static void printVector(double[] vector){
         System.out.print(Arrays.toString(vector));
     }
 
